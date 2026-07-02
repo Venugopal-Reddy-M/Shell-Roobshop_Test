912d6ff760589ac5372698095bf2d3fc70f10a4f
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-02 23:27:10 +0530

    mongoDB

--- mongoDB.sh
+++ mongoDB.sh
@@ -24,7 +24,7 @@ VALIDATE(){
     fi
 }
 
-cp mongo.repo /etc/yum.repos.d/mongo.repo $>>$LOGS_FILE
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGS_FILE
 VALIDATE $? "copying mongo repo"
 
 dnf install mongodb-org -y &>>$LOGS_FILE
@@ -33,7 +33,7 @@ VALIDATE $? "installing mongoDB"
 systemctl enable mongod &>>$LOGS_FILE
 VALIDATE $? "enable mongoDB"
 
-sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf $>>$LOGS_FILE
+sed -i 's/127.0.0.1/0.0.0.0/g' /etc/mongod.conf &>>$LOGS_FILE
 VALIDATE $? "Allowing remote"
 
 systemctl start mongod &>>$LOGS_FILE
