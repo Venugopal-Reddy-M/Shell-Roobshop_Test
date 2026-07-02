8a84b1f365ab8f042916ee1d77426a866499f079
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-02 22:22:28 +0530

    mongoDB

---
+++ mongoDB.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [$USERID -ne 0]; then
+   echo -e "$R please run the script root level $N" |  tee -a $LOGS_FILE
+   exit 1
+fi
+
+mkdir -p $LOGS_FOLDER
+VALIDATE(){
+    if [$1 -ne 0]; then
+        echo -e "$2....$R FAILUR $N" | tee -a $LOGS_FILE
+        exit 1
+    else 
+        echo -e "$2...$R SUCCESS $N" | tee -a $LOGS_FILE
+    fi
+}
+  
