0609c1784b5158bcbba39b7978b1b15d340ecb24
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-06 19:23:13 +0530

    nodejs

--- catalouge.sh
+++ catalouge.sh
@@ -37,7 +37,7 @@ id roboshop &>>LOGS_FILE
      useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>$LOGS_FILE
      VALIDATE $? "Add system user..."
    else
-     echo -e" Roboshop user already exit...$Y SKIPPING $N"
+     echo -e "Roboshop user already exit...$Y SKIPPING $N"
    fi
 mkdir -p /app &>>$LOGS_FILE
 VALIDATE $? "create app directory"
