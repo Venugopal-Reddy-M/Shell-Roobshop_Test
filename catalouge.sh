3e1c039caf0b09e3d1a3406b3d6c5870467766cb
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-06 15:25:18 +0530

    mongodb

---
+++ catalouge.sh
@@ -0,0 +1,23 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+if[ $USERID -ne 0 ]; then
+  echo "Run the root level"
+  exit 1
+fi
+
+VALIDATE(){
+    if [ $1 -ne 0] then 
+       echo "$2....FILURE"
+       exit 1
+    else
+       echo "$2...SUCCESS"
+}
+dnf module disable nodejs -y
+VALIDATE $? "diseable Nodejs ...."
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "enable nodejs-20 ....."
+# dnf install nodejs -y
+# VALIDATE $? "installing Nodejs ...."
