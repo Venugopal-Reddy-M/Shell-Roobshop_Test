1e3fb96d3f7cdf7294607cf9da2a95b7f5dbde5c
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-06 23:41:51 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -1,50 +1,43 @@
 #!/bin/bash/
 
 USERID=$(id -u)
-LOGS_FOLDER="/var/log/shell-roboshop"
-LOGS_FILE="$LOGS_FOLDER/$0.log"
 #CONFIC_REDIS=/etc/redis/redis.conf
-R="\e[31m"
-G="\e[32m"
-Y="\e[33m"
-N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
  echo "Run root level"
  exit 1
 fi
-
-mkdir -p "$LOGS_FOLDER"
-
+VALIDATE(){
     if [ $1 -ne 0 ]; then
-        echo "$2....FAILED" | tee -a $LOGS_FILE
+        echo "$2....FAILED"
         exit 1
     else
-     echo "$2....Success" | tee -a $LOGS_FILE
+     echo "$2....Success"
     fi
+    }
 
 if [ $? -ne 0 ]; then
-   dnf module disable redis -y &>>LOGS_FILE
+   dnf module disable redis -y
    VALIDATE $? "Disable redis..."
 else 
- echo -e "disable redis already....$Y Skiping $N"
+ echo "disable redis already"
 fi
 
 if [ $? -ne 0 ]; then
-  dnf module enable redis:7 -y &>>LOGS_FILE
+  dnf module enable redis:7 -y
   VALIDATE $? "Enable redis..."
 else
-  echo -e "Enable redis alredy...$Y skiping $N"
+  echo "Enable redis...alredy"
 fi  
 
-dnf install redis -y &>>LOGS_FILE
+dnf install redis -y 
 VALIDATE $? "Installing redis..." 
 
 #mkdir -p $CONFIC_REDIS
 
-sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf &>>LOGS_FILE
+sed -i -e 's/127.0.0.1/0.0.0.0/g' -e '/protected-mode/ c protected-mode no' /etc/redis/redis.conf
 VALIDATE $? "Allowing remote..."
 
-systemctl enable redis &>>LOGS_FILE
-systemctl start redis &>>LOGS_FILE
+systemctl enable redis 
+systemctl start redis 
 VALIDATE $? "enable and start.... "
