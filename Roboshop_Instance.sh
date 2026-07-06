a7a2cdc1f88f3c4db983a7cdb3e5a65c95a205be
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-06 21:24:51 +0530

    redis

--- Roboshop_Instance.sh
+++ Roboshop_Instance.sh
@@ -29,7 +29,7 @@ do
         RECORD_NAME="$instance.$DOMAIN_NAME" #mongodb.solohunting.online
     fi
 
-    echo IP Addess:"$IP"
+    echo "IP Addess: $IP"
     aws route53 change-resource-record-sets \
         --hosted-zone-id $ZONE_ID \
         --change-batch '
