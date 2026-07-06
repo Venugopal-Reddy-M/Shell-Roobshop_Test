8ca0c8bd6201dd2b8f45e158d66c4c52a401e4e4
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-06 22:09:33 +0530

    redis

--- redis.sh
+++ redis.sh
@@ -5,16 +5,15 @@ USERID=$(id -u)
 if [ $USERID -ne 0 ]; then
  echo "Run root level"
  exit 1
-if
-
-VALIDATE(){
- if [$1 -ne 0 ]; then
-    echo "$2....FAILED"
-    exix 1
-else
-   echo "$2....Success"
 fi
-}
+VALIDATE(){
+    if [$1 -ne 0 ]; then
+        echo "$2....FAILED"
+        exix 1
+    else
+    echo "$2....Success"
+    fi
+    }
 
 dnf module disable redis -y
 VALIDATE $? "Disable redis..."
