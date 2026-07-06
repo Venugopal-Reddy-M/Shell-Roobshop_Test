bd8dc8abb7a5c6929b060839f556cc70af1a4338
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-06 15:36:58 +0530

    nodejs

--- catalouge.sh
+++ catalouge.sh
@@ -1,6 +1,12 @@
 #!/bin/bash/
 
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
   echo "Run the root level"
@@ -9,16 +15,16 @@ fi
 
 VALIDATE(){
     if [ $1 -ne 0 ]; then 
-       echo "$2....FAILURE"
+       echo "$2....FAILURE" | tee -a $LOGS_FILE
        exit 1
     else
-       echo "$2...SUCCESS"
+       echo "$2...SUCCESS" | tee -a $LOGS_FILE
    fi
 }
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>$LOGS_FILE
 VALIDATE $? "diseable Nodejs ...."
 
-dnf module enable nodejs:20 -y
+dnf module enable nodejs:20 -y &>>$LOGS_FILE
 VALIDATE $? "enable nodejs-20 ....."
 # dnf install nodejs -y
 # VALIDATE $? "installing Nodejs ...." 
