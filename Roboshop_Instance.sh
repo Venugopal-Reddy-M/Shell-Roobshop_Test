702d1cd76f1ae470a023065e24dc023baa48b8a2
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-06 20:41:55 +0530

    nodejs

--- Roboshop_Instance.sh
+++ Roboshop_Instance.sh
@@ -14,8 +14,7 @@ do
     --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$instance}]" \
     --query 'Instances[0].InstanceId' \
     --output text )
-    
- #aws ec2 wait instance-running --instance-ids "$INSTANCE_ID"
+
 
     if [ "$instance" == "frontend" ]; then
         IP=$(aws ec2 describe-instances \
