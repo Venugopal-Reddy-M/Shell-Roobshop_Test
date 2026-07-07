2349ad86b21b416716bc6fe1a338de8f056b282c
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-07 21:31:17 +0530

    mysql

---
+++ mysql.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+if [ $USERID -ne 0 ]; then
+  echo "Run the root level"
+  exit 1
+fi
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+    echo "$2...FAILED"
+   else
+     echo "$2...SUCCESS"
+    fi 
+           
+}
+
+dnf install mysql-server -y
+systemctl enable mysqld
+systemctl start mysqld  
+VALIDATE $? "Install, enable and start mysql..."
