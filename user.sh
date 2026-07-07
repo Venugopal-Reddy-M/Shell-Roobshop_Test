ab2cd34dafb624e3aa248216441ef9e1838d77e1
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-07 17:23:36 +0530

    user-nodejs

---
+++ user.sh
@@ -0,0 +1,21 @@
+#!/bin/bash/
+
+USERID=$(id -u)
+
+if[ USERID -ne 0]; then
+  echo "Run the root level"
+  exit 1
+fi
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+    echo "$2...FAILED"
+   else
+     echo "$2...SUCCESS"
+    fi 
+           
+}
+
+dnf module disable nodejs -y
+VALIDATE $? "disable nodejs..."
+
