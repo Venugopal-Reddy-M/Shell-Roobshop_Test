788b4b99e2895718ff7fa57691b534e0f73beadf
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-07 17:48:43 +0530

    user-nodejs

--- user.sh
+++ user.sh
@@ -31,9 +31,13 @@ VALIDATE $? "disable AND Enable nodejs...."
 
 dnf install nodejs -y &>>LOGS_FILE
 VALIDATE $? "install nodejs"
-
-useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOGS_FILE
-VALIDATE $? "ADD robo user"
+id roboshop
+if [ $? -ne 0 ]; then
+   useradd --system --home /app --shell /sbin/nologin --comment "roboshop system user" roboshop &>>LOGS_FILE
+   VALIDATE $? "ADD robo user"
+else
+  echo "user already exit...$Y SKIPPING $N"
+fi
 
 mkdir /app &>>LOGS_FILE
 VALIDATE $? "Create app directory"
