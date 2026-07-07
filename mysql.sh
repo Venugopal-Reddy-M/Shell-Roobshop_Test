5f602e676e207e05200ab9ee9ddbf86960f4e2b7
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-07 21:37:39 +0530

    mysql

--- mysql.sh
+++ mysql.sh
@@ -26,3 +26,6 @@ dnf install mysql-server -y &>>LOGS_FILE
 systemctl enable mysqld &>>LOGS_FILE
 systemctl start mysqld  &>>LOGS_FILE
 VALIDATE $? "Install, enable and start mysql..."
+
+mysql_secure_installation --set-root-pass RoboShop@1 &>>LOGS_FILE
+VALIDATE $? "Setup root password"
