79364aaf6dc9c9bc39185b011f7e9085f90854dd
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-07 23:31:15 +0530

    frontend

---
+++ frontend.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+SCRIPT_DIR=$PWD
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+MYSQL_HOST=mysql.solohunting.online
+
+if [ $USERID -ne 0 ]; then
+  echo "Run the root level"
+  exit 1
+fi
+
+VALIDATE(){
+   if [ $1 -ne 0 ]; then
+    echo "$2...FAILED"
+   else
+     echo "$2...SUCCESS"
+    fi 
+           
+}
+
+dnf module list nginx &>>LOGS_FILE
+VALIDATE $? "install module list..."
+
+dnf module disable nginx -y &>>LOGS_FILE
+dnf module enable nginx:1.24 -y &>>LOGS_FILE
+dnf install nginx -y &>>LOGS_FILE
+VALIDATE $? "installing nginx.."
+
+systemctl enable nginx &>>LOGS_FILE
+systemctl start nginx &>>LOGS_FILE
+VALIDATE $? "Enable And Start nginx.."
+
+rm -rf /usr/share/nginx/html/* 
+VALIDATE $? "Remove default content"
+
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
+cd /usr/share/nginx/html 
+unzip /tmp/frontend.zip
+VALIDATE $? "download the forntend code..."
+
+rm -rf /etc/nginx/nginx.conf
+
+cp $SCRIPT_DIR/nginx.conf /etc/nginx/nginx.conf
+VALIDATE $? "createing systemctl service..."
+
+systemctl restart nginx 
+VALIDATE $? "restart nginx..."
