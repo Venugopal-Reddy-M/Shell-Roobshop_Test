d60cdbcfae1eafca639f372b5c18ac48d7aca833
Venugopal Reddy-M <venuaws.mvgr27@gmail.com>
2026-07-07 17:30:37 +0530

    user-nodejs

--- user.sh
+++ user.sh
@@ -1,11 +1,20 @@
 #!/bin/bash/
 
 USERID=$(id -u)
+LOGS_FOLDER="/var/log/shell-roboshop"
+LOGS_FILE="$LOGS_FOLDER/$0.log"
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
 
 if [ $USERID -ne 0 ]; then
   echo "Run the root level"
   exit 1
 fi
+
+mkdir -p $LOGS_FOLDER 
+
 
 VALIDATE(){
    if [ $1 -ne 0 ]; then
@@ -16,6 +25,6 @@ VALIDATE(){
            
 }
 
-dnf module disable nodejs -y
+dnf module disable nodejs -y &>>LOGS_FILE
 VALIDATE $? "disable nodejs..."
 
